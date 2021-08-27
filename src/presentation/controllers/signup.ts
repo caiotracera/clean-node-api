e24a4bdb7342e99af263b0a96b614ce064ef4b48
Caio Tracera <ctrcra@gmail.com>
2021-08-27 02:36:17 -0300

    refactor: make SignUpController conforms to a protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,11 +1,17 @@
 import { MissingParamError } from '../errors/missing-param-error';
 import { badRequest } from '../helpers/http-helper';
+import { IController } from '../protocols/controller';
 import { IHttpRequest, IHttpResponse } from '../protocols/http';
 
-export class SignUpController {
+export class SignUpController implements IController {
   // eslint-disable-next-line consistent-return
   handle(httpRequest: IHttpRequest): IHttpResponse {
-    const requiredFields = ['name', 'email', 'password'];
+    const requiredFields = [
+      'name',
+      'email',
+      'password',
+      'passwordConfirmation',
+    ];
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
